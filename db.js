c967f038536b7bb38062b53cb009944753a8141d
kantam5 <59176502+kantam5@users.noreply.github.com>
2020-03-04 04:18:25 +0900

    2.23

---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+  {
+    id: 11111,
+    title: "Video awesome",
+    description: "This is something",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 111111,
+      name: "Me",
+      email: "me@gmail.com"
+    }
+  },
+  {
+    id: 22222,
+    title: "Video nice",
+    description: "This is something",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 111111,
+      name: "Me",
+      email: "me@gmail.com"
+    }
+  },
+  {
+    id: 33333,
+    title: "Video perfect",
+    description: "This is something",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 111111,
+      name: "Me",
+      email: "me@gmail.com"
+    }
+  },
+  {
+    id: 44444,
+    title: "Video good",
+    description: "This is something",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 111111,
+      name: "Me",
+      email: "me@gmail.com"
+    }
+  }
+];
